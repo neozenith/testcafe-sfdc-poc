0b5b204cd2d988f0b46ce226bfb5eaf82b660b25
Josh Peak <neozenith.dev@gmail.com>
2018-02-13 15:38:12 +1100

    wip: trying to force TestCafe to load past the DOMContentLoaded and window.load events...

--- tests/index.js
+++ tests/index.js
@@ -2,12 +2,18 @@
 
 require('dotenv').config();
 
-const { Selector } = require('testcafe');
+const { Selector, ClientFunction } = require('testcafe');
 
 const landingPages = {
 	classic: ['/home/home.jsp', '/setup/forcecomHomepage.apexp'],
 	lightning: ['/one/one.app']
 };
+
+function sleep(ms) {
+	return new Promise(resolve => {
+		setTimeout(resolve, ms);
+	});
+}
 
 fixture('Test Org')
 	.page('https://login.salesforce.com/')
@@ -18,9 +24,21 @@ fixture('Test Org')
 			throw Error('TEST_ORG_PASSWORD environment variable not set in .env file');
 
 		await t
+			.setPageLoadTimeout(0)
 			.typeText('#username', process.env.TEST_ORG_USERNAME)
 			.typeText('#password', process.env.TEST_ORG_PASSWORD)
-			.click('#Login');
+			.click('#Login')
+			.wait(15000);
+
+		const location = await t.eval(() => window.location);
+
+		if (location.pathname === '/one/one.app') {
+			await t
+				.setPageLoadTimeout(0)
+				.navigateTo(location.origin + '/one/one.app#/home')
+				.wait(15000);
+			t.ctx.uiExperience = 'lightning';
+		}
 
 		const classicHeader = Selector('#phHeader');
 		const lightningHeader = Selector('#oneHeader');
@@ -28,10 +46,9 @@ fixture('Test Org')
 		// Await element in DOM and visibility check
 		const [classicHeaderElement, lightningHeaderElement] = await Promise.all([
 			classicHeader.with({ visibilityCheck: true })(),
-			lightningHeader.with({ visibilityCheck: true, timeout: 60000 })()
+			lightningHeader.with({ visibilityCheck: true })()
 		]);
 
-		t.ctx.uiExperience = null;
 		if (lightningHeaderElement) {
 			t.ctx.uiExperience = 'lightning';
 		}
@@ -39,10 +56,11 @@ fixture('Test Org')
 		if (classicHeaderElement && classicHeader.exists) {
 			t.ctx.uiExperience = 'classic';
 		}
+
 		await t.expect(t.ctx.uiExperience).notEql(null, 'Unknown UI Experience or failed to login');
 	});
 
-test('UI has Setup Link', async t => {
+test.only('UI has Setup Link', async t => {
 	let setupLinkSelector;
 	switch (t.ctx.uiExperience) {
 		case 'classic':
